1fce6fef9deb3ce62ffb732ef12b3b18995563c7
Julio <>
2017-10-16 20:39:10 -0600

    Firebase Config & Facebook Login

--- src/LoginView.js
+++ src/LoginView.js
@@ -16,9 +16,54 @@ import FBSDK, {
   AccessToken
 } from 'react-native-fbsdk'
 
+
+
 import { Actions } from 'react-native-router-flux';
 
+import * as firebase from "firebase";
+
+const config = {
+    apiKey: "AIzaSyDZyNuWfoLQ5ebU0xFpo8gkieOQ00Lp2lI",
+    authDomain: "appmusic-869c3.firebaseapp.com",
+    databaseURL: "https://appmusic-869c3.firebaseio.com",
+    projectId: "appmusic-869c3",
+    storageBucket: "",
+    messagingSenderId: "1006783840974"
+  };
+  firebase.initializeApp(config);
+
+
+const { FacebookAuthProvider } = firebase.auth
+const firebaseAuth = firebase.auth()
+
 export default class LoginView extends Component {
+
+  state = {
+    credentials: null
+  }
+
+
+
+  // Antes de que el componente este moentado
+  componentWillMount() {
+    this.authenticateUser()
+  }
+
+
+
+
+  authenticateUser = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      const { accessToken } = data
+      const credential = FacebookAuthProvider.credential(accessToken)
+      firebaseAuth.signInWithCredential(credential).then((credentials) => {
+        console.warn("Sign In Sucess", credentials);
+        this.setState({ credentials })
+      }, function(error){
+        console.warn("Sign In Error", error);
+      });
+    })
+  }
 
 handleLoginFinished = (error, result) => {
   if (error) {
@@ -26,12 +71,8 @@ handleLoginFinished = (error, result) => {
   } else if (result.isCancelled) {
     alert("login is cancelled.");
   } else {
-    AccessToken.getCurrentAccessToken().then(() => {
-      /*(data) => {
-        alert(data.accessToken.toString())
-      }*/
-      Actions.home()
-    })
+
+      this.authenticateUser()
   }
 }
 
@@ -61,7 +102,8 @@ handleLoginFinished = (error, result) => {
       </View>*/
 
       <View style={styles.container}>
-      <Text style={styles.welcome}>Bienvenidos a PlatziMusic</Text>
+      <Text style={styles.welcome}>Bienvenidos a AppMusic</Text>
+      <Text style={styles.welcome}>{this.state.credentials && this.state.credentials.displayName}</Text>
         <LoginButton
           publishPermissions={["publish_actions"]}
           readPermissions={['public_profile','email']}
